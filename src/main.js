43910da0df2aea8e28fff264df55e39ddc050758
CJ Ting <fatelovely1128@gmail.com>
2016-10-29 16:51:17 +0800

    project setup

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/*
+* @Author: CJ Ting
+* @Date: 2016-10-29 16:15:28
+* @Email: cj.ting@fugetech.com
+*/
+
+require("./main.styl")
+require("jquery")
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/*
+* @Author: CJ Ting
+* @Date: 2016-10-29 16:13:22
+* @Email: cj.ting@fugetech.com
+*/
+
+var path = require("path")
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.styl$/,
+        exclude: /node_modules/,
+        loader: "style!css!stylus",
+      }
+    ],
+  },
+}
